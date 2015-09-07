49cbb4914076dad2b25bb7c34a15b6a4d54dd51d
Mahmoud Ismail <maism@kth.se>
2015-09-07 11:33:05 +0200

    first commit

---
+++ src/main/java/se/kth/id1020/DoublingRatio.java
@@ -0,0 +1,78 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.kth.id1020;
+
+import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.introcs.StdRandom;
+
+public class DoublingRatio {
+
+  private interface SumHandler{
+    int count(int[] a);
+  }
+
+  private static double timeTrial(int N, SumHandler handler) {
+    int MAX = 1000000;
+    int[] a = new int[N];
+    for (int i = 0; i < N; i++) {
+      a[i] = StdRandom.uniform(-MAX, MAX);
+    }
+    Stopwatch timer = new Stopwatch();
+    int cnt = handler.count(a);
+    return timer.elapsedTime();
+  }
+
+  public static double timeTrialTwoSum(int N){
+    return timeTrial(N, new SumHandler() {
+      @Override
+      public int count(int[] a) {
+       return TwoSum.count(a);
+      }
+    });
+  }
+
+  public static double timeTrialTwoSumFast(int N){
+    return timeTrial(N, new SumHandler() {
+      @Override
+      public int count(int[] a) {
+        return TwoSumFast.count(a);
+      }
+    });
+  }
+
+
+  public static double timeTrialThreeSum(int N){
+    return timeTrial(N, new SumHandler() {
+      @Override
+      public int count(int[] a) {
+        return ThreeSum.count(a);
+      }
+    });
+  }
+
+
+  public static double timeTrialThreeSumFast(int N){
+    return timeTrial(N, new SumHandler() {
+      @Override
+      public int count(int[] a) {
+        return ThreeSumFast.count(a);
+      }
+    });
+  }
+
+}
---
+++ src/main/java/se/kth/id1020/RunningTimes.java
@@ -0,0 +1,50 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.kth.id1020;
+
+public class RunningTimes {
+
+  public static void main(String[] args){
+    if(args.length != 2){
+      System.out.println("Usage: java -jar runningtimes.jar Operation N");
+      System.out.println("Example: java -jar runningtimes.jar 2sum 1000");
+      System.out.println("Operation could be 2sum, 2sumfast, 3sum, 3sumfast");
+      System.exit(-1);
+    }
+    String operation = args[1];
+    int N = Integer.valueOf(args[2]);
+    double time = 0;
+    if(operation.equals("2sum")){
+      time = DoublingRatio.timeTrialTwoSum(N);
+    }else if(operation.equals("2sumfast")){
+      time = DoublingRatio.timeTrialTwoSumFast(N);
+    }else if(operation.equals("3sum")){
+      time = DoublingRatio.timeTrialThreeSum(N);
+    }else if(operation.equals("3sumfast")){
+      time = DoublingRatio.timeTrialThreeSumFast(N);
+    }else{
+      System.out.println("Wrong operation ["+operation+"] it must be " +
+          "one of the following [2sum, " +
+          "2sumfast, 3sum, 3sumfast]");
+      System.exit(-1);
+    }
+
+    System.out.println("Time taken for " + operation + " for input " + N + ":" +
+        time + " seconds");
+  }
+}
